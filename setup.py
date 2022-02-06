cf116233360ea13f1e38b8454b3762c65eb1a72d
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-02-06 17:32:22 +0300

    bump version → 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="jj-district42",
-    version="0.2.1",
+    version="1.0.0",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
